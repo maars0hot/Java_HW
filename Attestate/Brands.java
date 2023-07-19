acbc172900d69ec6c111ef6fd3218bbd44e23a81
maars0hot <127972939+maars0hot@users.noreply.github.com>
2023-07-19 21:08:16 +0600

    Attestate done

---
+++ Attestate/Brands.java
@@ -0,0 +1,23 @@
+package Attestate;
+
+public class Brands {
+    private String brand;
+    private String country;
+    private String language;
+
+    public Brands(String brand, String country, String language){
+        this.brand = brand;
+        this.country = country;
+        this.language = language;
+        }
+
+    public String getBrand() {return brand;}
+    public String getCountry() {return country;}
+    public String getLanguage() {return language;}
+
+    @Override
+    public String toString() {
+        return "Brand [brand= " + brand + ", country= " + country + ", language= " + language + "]";
+    }
+}
+
